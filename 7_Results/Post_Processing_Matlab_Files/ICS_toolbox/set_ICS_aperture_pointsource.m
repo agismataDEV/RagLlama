f228c490d5930b966bdc5b7283a503b0ebf078a1
Agisilaos Matalliotakis <agismatalliota@login1.hpc.tudelft.nl>
2021-07-01 21:39:16 +0200

    Delete old files

--- 7_Results/Post_Processing_Matlab_Files/ICS_toolbox/set_ICS_aperture_pointsource.m
+++
@@ -1,9 +0,0 @@
-function aperture = set_ICS_aperture_pointsource
-
-% aperture = set_ICS_aperture_pointsource 
-% Generates the aperture structure for a point source transducer. The
-% structure contains the aperture settings used the set_ICS_source_parametric function. 
-
-% JH, version 090116
-
-aperture = struct('aperturetype','pointsource');
