9c2a1f5ffb60841dcb91f9431c3f88d94c27937d
Agisilaos Matalliotakis <agismatalliota@login1.hpc.tudelft.nl>
2021-11-02 15:51:23 +0100

    Organize Folders

--- 7_RESULTS/Post-Processing Matlab Files/test.m
+++
@@ -1,6 +0,0 @@
-for i = 15:15
-A1 = importdata(['..\David_21deg_x_Lagrangian_Only_3\output_file_pressure',int2string_ICS(i),'.txt']);
-A2 = importdata(['..\David_21deg_x_Lagrangian_Only_2\output_file_pressure',int2string_ICS(i),'.txt']);
-sum(sum(A1-A2>1E-10))
-end
-
